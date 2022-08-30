c37fdd6f82dc8c3d6b3f458ce7d262b5b291efa2
Matheus Souza <matheus3864@gmail.com>
2022-08-30 09:25:56 -0300

    Start code

---
+++ tutorial/public/index.php
@@ -0,0 +1,60 @@
+<?php
+
+use Phalcon\Di\FactoryDefault;
+use Phalcon\Loader;
+use Phalcon\Mvc\View;
+use Phalcon\Mvc\Application;
+use Phalcon\Url;
+
+
+
+define('BASE_PATH', dirname(__DIR__));
+define('APP_PATH', BASE_PATH . '/app');
+// ...
+
+$loader = new Loader();
+
+$loader->registerDirs(
+    [
+        APP_PATH . '/controllers/',
+        APP_PATH . '/models/',
+    ]
+);
+
+$loader->register();
+
+// Create a DI
+$container = new FactoryDefault();
+
+$container->set(
+    'view',
+    function () {
+        $view = new View();
+        $view->setViewsDir(APP_PATH . '/views/');
+
+        return $view;
+    }
+);
+
+$container->set(
+    'url',
+    function () {
+        $url = new Url();
+        $url->setBaseUri('/');
+
+        return $url;
+    }
+);
+
+$application = new Application($container);
+
+try {
+    // Handle the request
+    $response = $application->handle(
+        $_SERVER["REQUEST_URI"]
+    );
+
+    $response->send();
+} catch (\Exception $e) {
+    echo 'Exception: ', $e->getMessage();
+}
